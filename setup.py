461f7b2011a36242d10b5ba88e48ce9926415dfd
Fox <51061164+TristanPFox@users.noreply.github.com>
2021-11-20 16:25:31 -0500

    v2.6

---
+++ setup.py
@@ -0,0 +1,15 @@
+import sys, os, shutil, glob
+
+print("=== DCS Waypoint Creator Updater ===")
+os.system("pip install pip --upgrade")
+os.system("pip install opencv-python")
+os.system("pip install easyocr")
+os.system("pip install pyautogui")
+os.system("pip install keyboard")
+os.system("pip install numpy")
+os.system("pip install matplotlib")
+os.system("pip install pyscreenshot")
+
+# End of line
+print("Finished Setup!")
+input("\n\nEnd of Line - Press Enter to Exit\n")
