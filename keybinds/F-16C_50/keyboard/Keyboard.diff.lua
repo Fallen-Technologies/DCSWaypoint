c4aad58d64bcac53e0b8cec4fc17daae7d2e236e
Fox <51061164+TristanPFox@users.noreply.github.com>
2022-01-17 16:54:29 -0500

    Added F-16C

---
+++ keybinds/F-16C_50/keyboard/Keyboard.diff.lua
@@ -0,0 +1,293 @@
+local diff = {
+	["keyDiffs"] = {
+		["d17pnilu18cdnilvdnilvpnilvunil"] = {
+			["name"] = "Cockpit panel view in",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num0",
+				},
+			},
+		},
+		["d3002pnilu3002cd17vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "0",
+				},
+			},
+			["name"] = "ICP Priority Function Button - 0(M-SEL)",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num0",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+		},
+		["d3003pnilu3003cd17vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "1",
+				},
+			},
+			["name"] = "ICP Priority Function Button - 1(T-ILS)",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num1",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+		},
+		["d3004pnilu3004cd17vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "2",
+				},
+			},
+			["name"] = "ICP Priority Function Button - 2/N(ALOW)",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num2",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+		},
+		["d3005pnilu3005cd17vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "3",
+				},
+			},
+			["name"] = "ICP Priority Function Button - 3",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num3",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+		},
+		["d3006pnilu3006cd17vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "4",
+				},
+			},
+			["name"] = "ICP Priority Function Button - 4/W(STPT)",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num4",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+		},
+		["d3007pnilu3007cd17vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "5",
+				},
+			},
+			["name"] = "ICP Priority Function Button - 5(CRUS)",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num5",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+		},
+		["d3008pnilu3008cd17vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "6",
+				},
+			},
+			["name"] = "ICP Priority Function Button - 6/E(TIME)",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num6",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+		},
+		["d3009pnilu3009cd17vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "7",
+				},
+			},
+			["name"] = "ICP Priority Function Button - 7(MARK)",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num7",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+		},
+		["d3010pnilu3010cd17vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "8",
+				},
+			},
+			["name"] = "ICP Priority Function Button - 8/S(FIX)",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num8",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+		},
+		["d3011pnilu3011cd17vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "9",
+				},
+			},
+			["name"] = "ICP Priority Function Button - 9(A-CAL)",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num9",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+		},
+		["d3012pnilu3012cd17vd1vpnilvu0"] = {
+			["name"] = "ICP COM Override Button - COM1(UHF)",
+			["removed"] = {
+				[1] = {
+					["key"] = "5",
+				},
+			},
+		},
+		["d3013pnilu3013cd17vd1vpnilvu0"] = {
+			["name"] = "ICP COM Override Button - COM2(VHF)",
+			["removed"] = {
+				[1] = {
+					["key"] = "6",
+				},
+			},
+		},
+		["d3014pnilu3014cd17vd1vpnilvu0"] = {
+			["name"] = "ICP IFF Override Button - IFF",
+			["removed"] = {
+				[1] = {
+					["key"] = "7",
+				},
+			},
+		},
+		["d3015pnilu3015cd17vd1vpnilvu0"] = {
+			["name"] = "ICP LIST Override Button - LIST",
+			["removed"] = {
+				[1] = {
+					["key"] = "8",
+				},
+			},
+		},
+		["d3016pnilu3016cd17vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Enter",
+				},
+			},
+			["name"] = "ICP Enter Button - ENTR",
+		},
+		["d3018pnilu3018cd17vd1vpnilvu0"] = {
+			["name"] = "ICP Master Mode Button - A-A",
+			["removed"] = {
+				[1] = {
+					["key"] = "1",
+				},
+			},
+		},
+		["d3019pnilu3019cd17vd1vpnilvu0"] = {
+			["name"] = "ICP Master Mode Button - A-G",
+			["removed"] = {
+				[1] = {
+					["key"] = "2",
+				},
+			},
+		},
+		["d3034pnilu3034cd17vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = ".",
+				},
+			},
+			["name"] = "ICP Data Control Switch - UP",
+		},
+		["d3035pnilu3035cd17vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = ",",
+				},
+			},
+			["name"] = "ICP Data Control Switch - DOWN",
+		},
+		["d3036pnilu3036cd16vd1vpnilvu0"] = {
+			["name"] = "RDR CURSOR Switch - Down",
+			["removed"] = {
+				[1] = {
+					["key"] = ".",
+				},
+			},
+		},
+		["d3037pnilu3037cd16vd1vpnilvu0"] = {
+			["name"] = "RDR CURSOR Switch - Left",
+			["removed"] = {
+				[1] = {
+					["key"] = ",",
+				},
+			},
+		},
+		["d3039pnilu3039cd16vd1vpnilvu0"] = {
+			["name"] = "ENABLE Switch - Depress",
+			["removed"] = {
+				[1] = {
+					["key"] = "Enter",
+				},
+			},
+		},
+		["d3044pnilunilcd16vd-1vpnilvunil"] = {
+			["name"] = "DOGFIGHT/Missile Override Switch - MISSILE OVERRIDE/CENTER",
+			["removed"] = {
+				[1] = {
+					["key"] = "4",
+				},
+			},
+		},
+		["d3044pnilunilcd16vd1vpnilvunil"] = {
+			["name"] = "DOGFIGHT/Missile Override Switch - DOGFIGHT/CENTER",
+			["removed"] = {
+				[1] = {
+					["key"] = "3",
+				},
+			},
+		},
+		["dnilp3034unilcd16vdnilvp-1vunil"] = {
+			["name"] = "ANT ELEV Knob - CCW",
+			["removed"] = {
+				[1] = {
+					["key"] = "-",
+				},
+			},
+		},
+	},
+}
+return diff
