860d05f04d340a40bbef662b4778414da4488210
Fox <51061164+TristanPFox@users.noreply.github.com>
2024-11-16 19:38:41 -0500

    v7.1.0
    • Added UH-60L Blackhawk

---
+++ keybinds/UH-60L/keyboard/Keyboard.diff.lua
@@ -0,0 +1,221 @@
+local diff = {
+	["keyDiffs"] = {
+		["d17pnilu18cdnilvdnilvpnilvunil"] = {
+			["name"] = "Cockpit panel view in",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num0",
+				},
+			},
+		},
+		["d3237pnilu3237cd23vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Y",
+				},
+			},
+			["name"] = "AN/ASN-128B Select Button KYBD",
+		},
+		["d3238pnilu3238cd23vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "I",
+				},
+			},
+			["name"] = "AN/ASN-128B Select Button LTR Left",
+		},
+		["d3239pnilu3239cd23vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "O",
+				},
+			},
+			["name"] = "AN/ASN-128B Select Button LTR MID",
+		},
+		["d3240pnilu3240cd23vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "P",
+				},
+			},
+			["name"] = "AN/ASN-128B Select Button LTR Right",
+		},
+		["d3242pnilu3242cd23vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num1",
+				},
+			},
+			["name"] = "AN/ASN-128B Select Button 1",
+		},
+		["d3243pnilu3243cd23vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num2",
+				},
+			},
+			["name"] = "AN/ASN-128B Select Button 2",
+		},
+		["d3244pnilu3244cd23vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num3",
+				},
+			},
+			["name"] = "AN/ASN-128B Select Button 3",
+		},
+		["d3246pnilu3246cd23vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num4",
+				},
+			},
+			["name"] = "AN/ASN-128B Select Button 4",
+		},
+		["d3247pnilu3247cd23vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num5",
+				},
+			},
+			["name"] = "AN/ASN-128B Select Button 5",
+		},
+		["d3248pnilu3248cd23vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num6",
+				},
+			},
+			["name"] = "AN/ASN-128B Select Button 6",
+		},
+		["d3250pnilu3250cd23vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num7",
+				},
+			},
+			["name"] = "AN/ASN-128B Select Button 7",
+		},
+		["d3251pnilu3251cd23vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num8",
+				},
+			},
+			["name"] = "AN/ASN-128B Select Button 8",
+		},
+		["d3252pnilu3252cd23vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num9",
+				},
+			},
+			["name"] = "AN/ASN-128B Select Button 9",
+		},
+		["d3254pnilu3254cd23vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num-",
+				},
+			},
+			["name"] = "AN/ASN-128B Select Button CLR",
+		},
+		["d3255pnilu3255cd23vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num0",
+				},
+			},
+			["name"] = "AN/ASN-128B Select Button 0",
+		},
+		["d3256pnilu3256cd23vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "U",
+				},
+			},
+			["name"] = "AN/ASN-128B Select Button ENT",
+		},
+		["dnilp210u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Up Right slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num9",
+				},
+			},
+		},
+		["dnilp211u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Down Right slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num3",
+				},
+			},
+		},
+		["dnilp212u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Down Left slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num1",
+				},
+			},
+		},
+		["dnilp213u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Up Left slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num7",
+				},
+			},
+		},
+		["dnilp3106unilcdnilvdnilvpnilvunil"] = {
+			["name"] = "Collective Decrease",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num-",
+				},
+			},
+		},
+		["dnilp32u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Left slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num4",
+				},
+			},
+		},
+		["dnilp33u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Right slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num6",
+				},
+			},
+		},
+		["dnilp34u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Up slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num8",
+				},
+			},
+		},
+		["dnilp35u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Down slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num2",
+				},
+			},
+		},
+		["dnilp36unilcdnilvdnilvpnilvunil"] = {
+			["name"] = "View Center",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num5",
+				},
+			},
+		},
+	},
+}
+return diff
